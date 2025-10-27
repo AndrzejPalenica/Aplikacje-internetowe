c7e4fa247a04101d390b4aa0d12fc9059c240864
Andrzej Palenica <palenica1231@onet.pl>
2025-10-27 09:40:33 +0100

    27.10.2025

--- 23.09.2025.js
+++ 23.09.2025.js
@@ -7,18 +7,34 @@ function Uczen(imie_u, wiek_u, klasa_u){
  
     //zachowania
     this.przedstaw_sie = function() {
-        document.writeln("Imie: " + this.imie + "<br> Wiek:" + this.wiek + "<br> Klasa:" + this.klasa);
+        document.writeln("Imie: " + this.imie + "<br> Wiek:" + this.wiek + "<br> Klasa:" + this.klasa + '<br>');
+    }
+    this.zmien_wiek = function(nowy_wiek){
+        this.wiek = nowy_wiek;
     }
 }
 // TWORZENIE OBIEKTU
-uczen1 = new Uczen('Maks',15,'1A');
+ var uczen1 = new Uczen('Maks',15,'1A');
+ var Andrzej = new Uczen('Andrzej', 17,'4I1');
 
 // WYWOŁYWANIE METODY OBIEKTU
 uczen1.przedstaw_sie();
-
+Andrzej.przedstaw_sie();
 
 // ZMIANA WARTOŚCI CECHY
 uczen1.wiek = 16;
+//Andrzej.wiek += 2;
+Andrzej.zmien_wiek(18);
+
+
+
+
+
+
+
+
+
+
 
 uczen1.przedstaw_sie();
 
