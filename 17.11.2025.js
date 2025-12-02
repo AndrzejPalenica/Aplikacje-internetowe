8c747461c1377654fdc6a829b33eb3d6c7d49812
Andrzej Palenica <palenica1231@onet.pl>
2025-12-02 14:57:17 +0100

    02.12.2025

--- 17.11.2025.js
+++ 17.11.2025.js
@@ -35,12 +35,19 @@
 // */
 
 
+
+
+
+
+
 // document.writeln(window.innerHeight + "px - wysokość<br>" + window.innerWidth + "px - szerokość<br>");
-
-
 // var okno = window.open("https://www.zsht.pl", 'moje_okno', 'height=600, width=600, top=200, left=400');
-
 // // window.setTimeout("okno.close()", 3000);
+
+
+
+
+
 
 
 // function sprawdz_okno(){
@@ -51,8 +58,12 @@
 // window.setInterval("sprawdz_okno()", 250);
 
 
-// var licznik = 0;
 
+
+
+
+
+// var licznik = 0;
 // function uwaga(){
 //     alert("NIE WCHODŹ NA STRONE");
 //     licznik++;
@@ -61,10 +72,10 @@
 //         window.clearInterval(inter);
 //     }
 // }
-
-
-
 // var inter = window.setInterval("uwaga()", 2000);
+
+
+
 
 
 
@@ -76,11 +87,7 @@
 // document.writeln("pathname " + location.pathname + "<br>");
 // document.writeln("port " + location.port + "<br>");
 
-
-
-
 // location.href = 'https://zsht.pl';
-
 // location.replace("https://zsht.pl");
 
 
@@ -91,6 +98,9 @@
 
 
 var blok1 = document.getElementById('div-1');
+
+
+
 
 // var tekst = prompt("Wprowdz tekst: ");
 // blok1.innerText = tekst;
@@ -132,16 +142,70 @@ var blok1 = document.getElementById('div-1');
 
 
 
-var obr = document.getElementById("obrazek");
-var i = 2;
-function zmien_obr(){
-    // var sciezka = "grafika/" + i +".jpg";
-    obr.setAttribute("src","grafika/" + i +".jpg");
-    // obr.setAttribute("src", sciezka);
-    i++;
-    if(i > 4){
-        i = 1;
-    }
-}
+// var obr = document.getElementById("obrazek");
+// var i = 2;
+// function zmien_obr(){
+//     // var sciezka = "grafika/" + i +".jpg";
+//     obr.setAttribute("src","grafika/" + i +".jpg");
+//     // obr.setAttribute("src", sciezka);
+//     i++;
+//     if(i > 4){
+//         i = 1;
+//     }
+// }
+// window.setInterval("zmien_obr()", 2000);
 
-window.setInterval("zmien_obr()", 2000);
+
+// var kolor = '';
+// var r = prompt("Podaj R (0-255)");
+// var g = prompt("Podaj G (0-255)");
+// var b = prompt("Podaj B (0-255)");
+// kolor = "rgb(" + r + " , " + g + " , " + b + ")"
+// blok1.style.backgroundColor = kolor;
+
+
+
+// blok1.style.backgroundColor = 'rgba(32, 165, 121, 1)';
+blok1.style.border = 'green 10px solid'
+
+
+
+// var kolor = '';
+// var r = parseInt(Math.random()*256)
+// var g = parseInt(Math.random()*256)
+// var b = parseInt(Math.random()*256)
+// kolor = "rgb(" + r + " , " + g + " , " + b + ")";
+// blok1.style.backgroundColor = kolor;
+
+
+
+
+
+
+
+
+
+function losuj_kolor(){
+    var kolor = '';
+    var r = parseInt(Math.random()*256)
+    var g = parseInt(Math.random()*256)
+    var b = parseInt(Math.random()*256)
+    kolor = "rgb(" + r + " , " + g + " , " + b + ")";
+    blok1.style.backgroundColor = kolor;
+
+
+    var kolor2 = '';
+    var r2 = parseInt(Math.random()*256)
+    var g2 = parseInt(Math.random()*256)
+    var b2 = parseInt(Math.random()*256)
+    kolor2 = "rgb(" + r2 + " , " + g2 + " , " + b2 + ")";
+    blok1.style.borderColor = kolor2;
+
+
+    var top  = parseInt(Math.random()*800);
+    var left = parseInt(Math.random()*1500);
+    blok1.style.position = "absolute";   
+    blok1.style.top = top + "px";
+    blok1.style.left = left + "px";
+}
+window.setInterval("losuj_kolor()",2500);
