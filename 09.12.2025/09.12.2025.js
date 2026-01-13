86a592e485f58460ac35a9b799cbf1dc92e5fa19
Andrzej Palenica <palenica1231@onet.pl>
2026-01-13 14:55:56 +0100

    13.06.2026

--- 09.12.2025/09.12.2025.js
+++ 09.12.2025/09.12.2025.js
@@ -43,32 +43,62 @@
 
 
 
-var element = document.createElement("div");
+// var element = document.createElement("div");
 
-element.innerText = "NOWY DIV";
+// element.innerText = "NOWY DIV";
 
-var div_blok =  document.getElementById("blok");
+// var div_blok =  document.getElementById("blok");
 
-// div_blok.prepend(element);
-div_blok.after(element);
+// // div_blok.prepend(element);
+// div_blok.after(element);
 
-div_blok.style.backgroundColor =   "yellow";
-//priorytet taki jak styl wpisany(priorytet większy od arkusza zewnętrznego)
-
-
-// div_blok.classList.add("klasa_1");
-// div_blok.classList.add("klasa_2");
-
-div_blok.classList.add("klasa_1","klasa_2");
-div_blok.classList.remove("klasa_2");
+// div_blok.style.backgroundColor =   "yellow";
+// //priorytet taki jak styl wpisany(priorytet większy od arkusza zewnętrznego)
 
 
-// div_blok.classList.toggle("klasa_2");
-alert(div_blok.classList);
+// // div_blok.classList.add("klasa_1");
+// // div_blok.classList.add("klasa_2");
 
-div_blok.classList.contains("klasa_1"); 
+// div_blok.classList.add("klasa_1","klasa_2");
+// div_blok.classList.remove("klasa_2");
 
 
-// div_blok.id = "";
+// // div_blok.classList.toggle("klasa_2");
+// alert(div_blok.classList);
 
-element.id = "id_1"
+// div_blok.classList.contains("klasa_1"); 
+
+
+// // div_blok.id = "";
+
+// element.id = "id_1"
+
+
+
+
+
+
+
+
+
+
+var blok = document.getElementById("blok");
+
+var lista = document.createElement("ul");
+blok.append(lista);
+var x = prompt("Wprowadz liczbe elementów");
+for(i = 1; i <= x; i++){
+    var list_li = document.createElement("li");
+    list_li.innerText = "Pozycja" + i;
+    lista.append(list_li);
+    if(i%2 == 0 && i%10 != 0){
+        list_li.classList.add("klasa_even");
+    }
+    if(i%2 != 0){
+        list_li.classList.add("klasa_odd");
+    }
+    if(i%10 == 0){
+        list_li.classList.add("klasa10");
+    }
+}
+
